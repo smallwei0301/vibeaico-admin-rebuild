00bde3d4b362b1e97b2ce84a87cedafa70c71305
smallwei0301 <smallwei0301@gmail.com>
2026-08-31 12:19:56 +0800

    chore(agent): preserve control and issue provenance policy

--- src/components/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import { common } from '@/i18n/zh-TW/common';
-
-export function Footer() {
-  return (
-    <footer className="px-content py-4 text-center text-xs text-secondary">
-      {common.copyright}
-    </footer>
-  );
-}
